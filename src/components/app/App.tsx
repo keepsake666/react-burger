8b7ee85447cd1f16175720c3f8be904c34dda2ee
Sergey Veselov <veselovsa@list.ru>
2022-06-16 14:08:12 +0200

    start

---
+++ src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
