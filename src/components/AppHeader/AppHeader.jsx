fd99b6d94e2e8e952582c4756bda9cb1d70770e1
Sergey Veselov <veselovsa@list.ru>
2022-06-20 12:00:43 +0200

    feat: added css header

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
-import  styles from'./AppHeader.module.css'
+import styles from './AppHeader.module.css'
 import { Logo, BurgerIcon, ListIcon, ProfileIcon, Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 function AppHeader() {
-  const [current, setCurrent] = React.useState('one')
+
 
   return (
     <header className={styles.header}>
-      <div  style={{ display: 'flex' }}>
-      <Tab value="one"  onClick={setCurrent}>
-        <div style={{ display: 'flex' }}>
-          <BurgerIcon />
-          Конструктор
+      <div className={styles.header__container}>
+        <div className={styles.block}>
+          <div  style ={{display:'flex'}} className='mb-4 mt-4 pb-4 pt-4 pr-5'>
+            <BurgerIcon />
+            <p className="text text_type_main-default block ml-2">Конструктор</p>
+          </div>
+          <div style ={{display:'flex'}} className='mb-4 mt-4 ml-2 pb-4 pt-4 pl-5 pr-5' >
+            <ListIcon type="secondary" />
+            <p className="text text_type_main-default text_color_inactive ml-2">Лента заказов</p>
+          </div>
         </div>
-      </Tab>
-      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-        <div style={{ display: 'flex' }}>
-          <ListIcon />
-          Лента заказов
+        <Logo />
+        <div style ={{display:'flex'}} className='mb-4 mt-4 pb-4 pt-4 pl-5'>
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет </p>
         </div>
-      </Tab>
       </div>
-      <Logo />
-      <Tab value="three"  onClick={setCurrent}>
-        <div style={{ display: 'flex' }}>
-          <ProfileIcon />
-          Личный кабинет
-        </div>
-      </Tab>
-
     </header>
   );
 };
